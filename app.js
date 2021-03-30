29c65b1a47c4855c53fd585eeea293304f64d453
ganzorig-maker <79094997+ganzorig-maker@users.noreply.github.com>
2021-03-30 04:06:24 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,129 +1,405 @@
-// Тоглоомын бүх газарт ашиглагдах глобаль хувьсагчид
+// Дэлгэцтэй ажиллах контроллер
+var uiController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+    incomeList: ".income__list",
+    expenseList: ".expenses__list",
+    tusuvLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expeseLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
+    containerDiv: ".container",
+    expensePercentageLabel: ".item__percentage",
+    dateLabel: ".budget__title--month"
+  };
 
-var isNewGame;
-
-// Аль тоглогч шоо шидэх вэ гэдгийг энд хадгална
-var activePlayer;
-
-// Хоёр тоглогчийн цуглуулсан оноонууд
-var scores;
-
-// Идэвхтэй тоглогчийн цуглуулж байгаа ээлжийн оноо
-var roundScore;
-
-// Шооны зургийг үзүүлэх элементийг Дом оос хайж олоод энд хадгална
-var diceDom = document.querySelector(".dice");
-
-// Тоглоомыг эхлүүлнэ
-initGame();
-
-// ********************************************************************************ТОГЛООМЫГ ШИНЭЭР ЭХЛҮҮЛЭХ БУЮУ new game ТОВЧЛУУР**************
-function initGame() {
-  // Тоглоом эхлэлээ гэдэг төлөвт оруулна
-  isNewGame = true;
-
-  //  // Тоглогчдын ээлжийг хадгалах хувьсагч 
-  activePlayer = 0;
-
-  // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
-  scores = [0, 0];
-
-  // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
-  roundScore = 0;
-  // New game товчлууран дээр дарагдахад тоглоом дахин эхлэнэ
-document.querySelector(".btn-new").addEventListener("click", initGame);
-
-  // Програм эхлэхэд бэлтгэе
-  // ***********Дом руу хандах***********
-  document.getElementById("score-0").textContent = "0";
-  document.getElementById("score-1").textContent = "0";
-  document.getElementById("current-0").textContent = "0";
-  document.getElementById("current-1").textContent = "0";
-
-  // Тоглогчдын нэр
-  document.getElementById("name-0").textContent = "Player 1";
-  document.getElementById("name-1").textContent = "Player 2";
-
-  document.querySelector(".player-0-panel").classList.remove("winner");
-  document.querySelector(".player-1-panel").classList.remove("winner");
-  // Улаан цэгийг шилжүүлэх
-  document.querySelector(".player-0-panel").classList.remove("active");
-  document.querySelector(".player-1-panel").classList.remove("active");
-
-  document.querySelector(".player-0-panel").classList.add("active");
-
-  diceDom.style.display = "none";
-}
-
-// ********************************************************************************ШООГ ШИДЭХ ЭВЕНТ ЛИСТЕНЕР БУЮУ roll-dice ТОВЧЛУУР***********************
-document.querySelector(".btn-roll").addEventListener("click", function roll() {
-  if (isNewGame) {
-    // 1 ээс 6 хүртэлх санамсаргүй нэг тоо гаргаж авна
-    var diceNumber = Math.floor(Math.random() * 6) + 1;
-
-    // Шооны зургийг вэб дээр гаргах
-    diceDom.style.display = "block";
-
-    //Буусан санамсаргүй тоог харгалзах шооны зурагтай вэб дээр гаргаж авна
-    diceDom.src = "dice-" + diceNumber + ".png";
-
-    // Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүл
-    // ************* Тоглогчийн ээлжийг солих*****************
-    if (diceNumber !== 1) {
-      roundScore = roundScore + diceNumber;
-      document.getElementById(
-        "current-" + activePlayer
-      ).textContent = roundScore;
-    } else {
-      // 1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө
-      switchToNextPlayer();
+  var nodeListForeach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
     }
-  } 
-});
+  };
 
-// ********************************************************************************HOLD ТОВЧНЫ ЭВЕНТ ЛИСТЕНЕР БУЮУ ТОГЛОГЧИЙН ОНООГ ЦУГЛУУЛАХ************
-document.querySelector(".btn-hold").addEventListener("click", function hold() {
-  if (isNewGame) {
-    // уг тоглогчийн ээлжийн оноог глобаль хувьсагч дээр нэмж өгнө
-    scores[activePlayer] = scores[activePlayer] + roundScore;
+  var formatMoney = function(too, type) {
+    too = "" + too;
+    var x = too
+      .split("")
+      .reverse()
+      .join("");
 
-    // дэлгэц дээр нь оноог нь өөрчил
-    document.getElementById("score-" + activePlayer).textContent =
-      scores[activePlayer];
+    var y = "";
+    var count = 1;
 
-    // Уг тоглогчын хожсон эсэхийг шалгах
-    if (scores[activePlayer] >= 50) {
-      // Тоглоомыг дууссан төлөвт оруулна
-      isNewGame = false; 
-      alert("Тоглоом дууссан тул NEW GAME товчийг дарж дахин эхлүүлнэ үү");
-      
+    for (var i = 0; i < x.length; i++) {
+      y = y + x[i];
 
-      // ялагч гэсэн текстийг нэрнийх нь оронд гаргана
-      document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
-      document
-        .querySelector(".player-" + activePlayer + "-panel")
-        .classList.add("winner");
-      document
-        .querySelector(".player-" + activePlayer + "-panel")
-        .classList.remove("active");
-    } else {
-      // 1 буусан тул ээлжийг дараагын тоглогч руу шилжүүл
-      switchToNextPlayer();
+      if (count % 3 === 0) y = y + ",";
+      count++;
     }
-  } 
-});
 
-//*****************Ээлжээ солих функц******************
-function switchToNextPlayer() {
-  roundScore = 0;
-  document.getElementById("current-" + activePlayer).textContent = 0;
-  // if ийг өөрөөр ингэж бичнэ
-  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    var z = y
+      .split("")
+      .reverse()
+      .join("");
 
- 
-  document.querySelector(".player-0-panel").classList.toggle("active");
-  document.querySelector(".player-1-panel").classList.toggle("active");
-  diceDom.style.display = "none";
-}
+    if (z[0] === ",") z = z.substr(1, z.length - 1);
 
+    if (type === "inc") z = "+ " + z;
+    else z = "- " + z;
 
+    return z;
+  };
+
+  return {
+    displayDate: function() {
+      var unuudur = new Date();
+
+      document.querySelector(DOMstrings.dateLabel).textContent =
+        unuudur.getFullYear() + " оны " + unuudur.getMonth() + " сарын ";
+    },
+
+    changeType: function() {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType +
+          ", " +
+          DOMstrings.inputDescription +
+          ", " +
+          DOMstrings.inputValue
+      );
+
+      nodeListForeach(fields, function(el) {
+        el.classList.toggle("red-focus");
+      });
+
+      document.querySelector(DOMstrings.addBtn).classList.toggle("red");
+      // location = "http://1234.mn/course";
+    },
+
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // exp, inc
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: parseInt(document.querySelector(DOMstrings.inputValue).value)
+      };
+    },
+
+    displayPercentages: function(allPercentages) {
+      // Зарлагын NodeList-ийг олох
+      var elements = document.querySelectorAll(
+        DOMstrings.expensePercentageLabel
+      );
+
+      // Элемент болгоны хувьд зарлагын хувийг массиваас авч шивж оруулах
+      nodeListForeach(elements, function(el, index) {
+        el.textContent = allPercentages[index];
+      });
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    },
+
+    clearFields: function() {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputDescription + ", " + DOMstrings.inputValue
+      );
+
+      // Convert List to Array
+      var fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(el, index, array) {
+        el.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
+    tusviigUzuuleh: function(tusuv) {
+      var type;
+      if (tusuv.tusuv > 0) type = "inc";
+      else type = "exp";
+
+      document.querySelector(DOMstrings.tusuvLabel).textContent = formatMoney(
+        tusuv.tusuv,
+        type
+      );
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatMoney(
+        tusuv.totalInc,
+        "inc"
+      );
+      document.querySelector(DOMstrings.expeseLabel).textContent = formatMoney(
+        tusuv.totalExp,
+        "exp"
+      );
+
+      if (tusuv.huvi !== 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi + "%";
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi;
+      }
+    },
+
+    deleteListItem: function(id) {
+      var el = document.getElementById(id);
+      el.parentNode.removeChild(el);
+    },
+
+    addListItem: function(item, type) {
+      // Орлого зарлагын элементийг агуулсан html-ийг бэлтгэнэ.
+      var html, list;
+      if (type === "inc") {
+        list = DOMstrings.incomeList;
+        html =
+          '<div class="item clearfix" id="inc-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
+      } else {
+        list = DOMstrings.expenseList;
+        html =
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+      // Тэр HTML дотроо орлого зарлагын утгуудыг REPLACE ашиглаж өөрчилж
+      html = html.replace("%id%", item.id);
+      html = html.replace("$$DESCRIPTION$$", item.description);
+      html = html.replace("$$VALUE$$", formatMoney(item.value, type));
+
+      // Бэлтгэсэн HTML ээ DOM руу хийж өгнө.
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
+    }
+  };
+})();
+
+// Санхүүтэй ажиллах контроллер
+var financeController = (function() {
+  // private data
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // private data
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+    this.percentage = -1;
+  };
+
+  Expense.prototype.calcPercentage = function(totalIncome) {
+    if (totalIncome > 0)
+      this.percentage = Math.round((this.value / totalIncome) * 100);
+    else this.percentage = 0;
+  };
+
+  Expense.prototype.getPercentage = function() {
+    return this.percentage;
+  };
+
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.items[type].forEach(function(el) {
+      sum = sum + el.value;
+    });
+
+    data.totals[type] = sum;
+  };
+
+  // private data
+  var data = {
+    items: {
+      inc: [],
+      exp: []
+    },
+
+    totals: {
+      inc: 0,
+      exp: 0
+    },
+
+    tusuv: 0,
+
+    huvi: 0
+  };
+
+  return {
+    tusuvTootsooloh: function() {
+      // Нийт орлогын нийлбэрийг тооцоолно
+      calculateTotal("inc");
+
+      // Нийт зарлагын нийлбэрийг тооцоолно
+      calculateTotal("exp");
+
+      // Төсвийг шинээр тооцоолно
+      data.tusuv = data.totals.inc - data.totals.exp;
+
+      // Орлого зарлагын хувийг тооцоолно
+      if (data.totals.inc > 0)
+        data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100);
+      else data.huvi = 0;
+    },
+
+    calculatePercentages: function() {
+      data.items.exp.forEach(function(el) {
+        el.calcPercentage(data.totals.inc);
+      });
+    },
+
+    getPercentages: function() {
+      var allPercentages = data.items.exp.map(function(el) {
+        return el.getPercentage();
+      });
+
+      return allPercentages;
+    },
+
+    tusviigAvah: function() {
+      return {
+        tusuv: data.tusuv,
+        huvi: data.huvi,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp
+      };
+    },
+
+    deleteItem: function(type, id) {
+      var ids = data.items[type].map(function(el) {
+        return el.id;
+      });
+
+      var index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.items[type].splice(index, 1);
+      }
+    },
+
+    addItem: function(type, desc, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.items[type].push(item);
+
+      return item;
+    },
+
+    seeData: function() {
+      return data;
+    }
+  };
+})();
+
+// Програмын холбогч контроллер
+var appController = (function(uiController, financeController) {
+  var ctrlAddItem = function() {
+    // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+    var input = uiController.getInput();
+
+    if (input.description !== "" && input.value !== "") {
+      // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+      var item = financeController.addItem(
+        input.type,
+        input.description,
+        input.value
+      );
+
+      // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+      uiController.addListItem(item, input.type);
+      uiController.clearFields();
+
+      // Төсвийг шинээр тооцоолоод дэлгэцэнд үзүүлнэ.
+      updateTusuv();
+    }
+  };
+
+  var updateTusuv = function() {
+    // 4. Төсвийг тооцоолно
+    financeController.tusuvTootsooloh();
+
+    // 5. Эцсийн үлдэгдэл,
+    var tusuv = financeController.tusviigAvah();
+
+    // 6. Төсвийн тооцоог дэлгэцэнд гаргана.
+    uiController.tusviigUzuuleh(tusuv);
+
+    // 7. Элементүүдийн хувийг тооцоолно
+    financeController.calculatePercentages();
+
+    // 8. Элементүүдийн хувийг хүлээж авна
+    var allPercentages = financeController.getPercentages();
+
+    // 9. Эдгээр хувийг дэлгэцэнд гаргана.
+    uiController.displayPercentages(allPercentages);
+  };
+
+  var setupEventListeners = function() {
+    var DOM = uiController.getDOMstrings();
+
+    document.querySelector(DOM.addBtn).addEventListener("click", function() {
+      ctrlAddItem();
+    });
+
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+
+    document
+      .querySelector(DOM.inputType)
+      .addEventListener("change", uiController.changeType);
+
+    document
+      .querySelector(DOM.containerDiv)
+      .addEventListener("click", function(event) {
+        var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        if (id) {
+          // inc-2
+          var arr = id.split("-");
+          var type = arr[0];
+          var itemId = parseInt(arr[1]);
+
+          console.log(type + " ===> " + itemId);
+
+          // 1. Санхүүгийн модулиас type, id ашиглаад устгана.
+          financeController.deleteItem(type, itemId);
+
+          // 2. Дэлгэц дээрээс энэ элементийг устгана
+          uiController.deleteListItem(id);
+
+          // 3. Үлдэгдэл тооцоог шинэчилж харуулна.
+          // Төсвийг шинээр тооцоолоод дэлгэцэнд үзүүлнэ.
+          updateTusuv();
+        }
+      });
+  };
+
+  return {
+    init: function() {
+      console.log("Application started...");
+      uiController.displayDate();
+      uiController.tusviigUzuuleh({
+        tusuv: 0,
+        huvi: 0,
+        totalInc: 0,
+        totalExp: 0
+      });
+      setupEventListeners();
+    }
+  };
+})(uiController, financeController);
+
+appController.init();
